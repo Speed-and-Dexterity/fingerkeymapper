7fd4c3c4f5c00c4f056e2de9bdb7ba789df1472a
t3chy <elam.day.friedland@gmail.com>
2020-09-12 23:09:48 -0700

    it sucks

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ filter = False
 
 img = cv2.imread("keyboard.jpg")
 template = cv2.imread("qkey.png")
-w, h = template.shape[:-1]
+w = template.shape[1]
+h = template.shape[0]
 img.astype(np.float32)
 
 # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -22,11 +23,15 @@ img.astype(np.float32)
 # gray = cv2.adaptiveThreshold(gray, 255,  cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 7, 4)
 #gray.astype(np.float32)
 res = cv2.matchTemplate(img,template,cv2.TM_CCOEFF_NORMED)
-min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
-top_left = max_loc
-bottom_right = (top_left[0] + w, top_left[1] + h)
-print(top_left)
-print(bottom_right)
-cv2.rectangle(img,top_left, bottom_right, 255)
-cv2.imshow("g",img)
-cv2.waitKey()
+threshold = 0.5
+loc = np.where( res >= threshold)
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(img, pt, (pt[0] + w, pt[1] + h), (0,0,255), 10)
+# min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+# top_left = max_loc
+# bottom_right = (top_left[0] + w, top_left[1] + h)
+# print(top_left)
+# print(bottom_right)
+# print(img.shape)
+# cv2.rectangle(img,top_left, bottom_right, 255,8)
+cv2.imwrite("g.jpg",img)
